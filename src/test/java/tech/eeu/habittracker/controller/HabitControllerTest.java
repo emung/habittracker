7602e6aec50adab5beac521cc83a2a723e89f7b1
emung <emanuel.ungheanu@hotmail.com>
2023-10-18 20:36:57 +0200

    Issue#10: added more unit tests

--- src/test/java/tech/eeu/habittracker/controller/HabitControllerTest.java
+++ src/test/java/tech/eeu/habittracker/controller/HabitControllerTest.java
@@ -33,22 +33,18 @@ class HabitControllerTest {
         List<HabitDto> resultList = habitController.getAllHabits().getBody();
 
         assertNotNull(resultList);
-        assertEquals(2, resultList.size());
+        assertEquals(habitDtoList.size(), resultList.size());
         assertTrue(resultList.contains(habitDto1));
         assertTrue(resultList.contains(habitDto2));
-        assertEquals(habitDto1.getName(), resultList.get(0).getName());
-        assertEquals(habitDto2.getName(), resultList.get(1).getName());
-        assertEquals(habitDto1.getCategory(), resultList.get(0).getCategory());
-        assertEquals(habitDto2.getCategory(), resultList.get(1).getCategory());
-        assertEquals(habitDto1.getDescription(), resultList.get(0).getDescription());
-        assertEquals(habitDto2.getDescription(), resultList.get(1).getDescription());
+        assertEquals(habitDto1, resultList.get(0));
+        assertEquals(habitDto2, resultList.get(1));
     }
 
     @Test
     public void testGetHabitById() {
         HabitDto habitDto = new HabitDto(5L, "Learn Java", "Learn Java every day", "Programming");
 
-        when(habitFacade.getHabitById(habitDto.getId())).thenReturn(habitDto);
+        when(habitFacade.getHabitById(5L)).thenReturn(habitDto);
         HabitDto resultHabitDto = habitController.getHabitById(5L).getBody();
 
         assertNotNull(resultHabitDto);
@@ -56,6 +52,35 @@ class HabitControllerTest {
         assertEquals(habitDto.getName(), resultHabitDto.getName());
         assertEquals(habitDto.getDescription(), resultHabitDto.getDescription());
         assertEquals(habitDto.getCategory(), resultHabitDto.getCategory());
+    }
+
+    @Test
+    public void testGetHabitsByCategory() {
+        final String CATEGORY_STRING = "Category 1";
+        HabitDto habitDto1 = new HabitDto(1L, "Habit_1", "Habit_Description_1", "Category 1");
+        HabitDto habitDto2 = new HabitDto(2L, "Habit_2", "Habit_Description_2", "Category 1");
+        List<HabitDto> habitDtoList = new ArrayList<>(List.of(habitDto1, habitDto2));
+
+        when(habitFacade.getAllHabitsByCategory(CATEGORY_STRING)).thenReturn(habitDtoList);
+        List<HabitDto> resultList = habitController.getHabitsByCategory(CATEGORY_STRING).getBody();
+
+        assertNotNull(resultList);
+        assertEquals(habitDtoList.size(), resultList.size());
+        assertTrue(resultList.stream().allMatch(habitDto -> CATEGORY_STRING.equals(habitDto.getCategory())));
+    }
+
+    @Test
+    public void testGetHabitsByCategoryWhenCategoryIsNull() {
+        HabitDto habitDto1 = new HabitDto(1L, "Habit_1", "Habit_Description_1", "Category 1");
+        HabitDto habitDto2 = new HabitDto(2L, "Habit_2", "Habit_Description_2", "Category 2");
+        List<HabitDto> habitDtoList = new ArrayList<>(List.of(habitDto1, habitDto2));
+
+        when(habitFacade.getAllHabitsByCategory("")).thenReturn(habitDtoList);
+        List<HabitDto> resultList = habitController.getHabitsByCategory(null).getBody();
+
+        assertNotNull(resultList);
+        assertEquals(habitDtoList.size(), resultList.size());
+        assertNotEquals(resultList.get(0).getCategory(), resultList.get(1).getCategory());
     }
 
 }
