51fe668844900a93ce00075ab76981e64d27f46d
emung <emanuel.ungheanu@hotmail.com>
2023-11-09 16:27:35 +0100

    NOTASK remove unit tests, use REST-assured for response validation

--- src/test/java/tech/eeu/habittracker/controller/HabitControllerTest.java
+++ src/test/java/tech/eeu/habittracker/controller/HabitControllerTest.java
@@ -1,151 +1,107 @@
 package tech.eeu.habittracker.controller;
 
-import org.junit.jupiter.api.Disabled;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.module.mockmvc.response.MockMvcResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.context.WebApplicationContext;
 import tech.eeu.habittracker.dto.HabitDto;
-import tech.eeu.habittracker.exception.HabitNotFoundException;
 import tech.eeu.habittracker.facade.HabitFacade;
-import tech.eeu.habittracker.request.CreateHabitRequest;
-import tech.eeu.habittracker.request.UpdateHabitRequest;
+import tech.eeu.habittracker.model.TargetPeriod;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
-@SuppressWarnings("unchecked")
 @ExtendWith(MockitoExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
 class HabitControllerTest {
 
-    @InjectMocks
-    HabitController habitController;
+    @Autowired
+    WebApplicationContext webApplicationContext;
 
-    @Mock
+    @MockBean
     HabitFacade habitFacade;
 
-    @Test
-    public void testGetAllHabits() {
-        HabitDto habitDto1 = new HabitDto(1L, "Habit_1", "Habit_Description_1", "Habit_Category_1");
-        HabitDto habitDto2 = new HabitDto(2L, "Habit_2", "Habit_Description_2", "Habit_Category_2");
-        List<HabitDto> habitDtoList = new ArrayList<>(List.of(habitDto1, habitDto2));
+    private static List<HabitDto> HABIT_DTOS;
 
-        when(habitFacade.getAllHabits()).thenReturn(habitDtoList);
-        List<HabitDto> resultList = habitController.getAllHabits().getBody();
+    private static HabitDto HABIT_DTO;
 
-        assertNotNull(resultList);
-        assertEquals(habitDtoList.size(), resultList.size());
-        assertTrue(resultList.contains(habitDto1));
-        assertTrue(resultList.contains(habitDto2));
-        assertEquals(habitDto1, resultList.get(0));
-        assertEquals(habitDto2, resultList.get(1));
+    private static final String CATEGORY = "Main category2";
+
+    @BeforeEach
+    public void initializeWebAppContext() {
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
+    }
+
+    @BeforeAll
+    public static void initializeHabitDtoList() {
+        HABIT_DTOS = new ArrayList<>(List.of(
+                new HabitDto(1L, "Habit name1", "Habit description1", "Main category1", 3, TargetPeriod.DAILY, 1),
+                new HabitDto(2L, "Habit name2", "Habit description2", "Main category1", 3, TargetPeriod.DAILY, 1),
+                new HabitDto(3L, "Habit name3", "Habit description3", "Main category2", 3, TargetPeriod.DAILY, 1),
+                new HabitDto(4L, "Habit name4", "Habit description4", "Main category2", 3, TargetPeriod.DAILY, 1)
+        ));
+
+        HABIT_DTO = new HabitDto(1L, "Habit name1", "Habit description1", "Main category1", 3, TargetPeriod.DAILY, 1);
     }
 
     @Test
-    public void testGetHabitById() {
-        HabitDto habitDto = new HabitDto(5L, "Learn Java", "Learn Java every day", "Programming");
+    @DisplayName("GET all habits")
+    public void shouldReturnAllHabits() {
+        when(habitFacade.getAllHabits()).thenReturn(HABIT_DTOS);
 
-        when(habitFacade.getHabitById(5L)).thenReturn(habitDto);
-        HabitDto resultHabitDto = habitController.getHabitById(5L).getBody();
+        MockMvcResponse response = RestAssuredMockMvc.get("/habits/all");
+        response.then().status(HttpStatus.OK);
 
-        assertNotNull(resultHabitDto);
-        assertEquals(habitDto.getId(), resultHabitDto.getId());
-        assertEquals(habitDto.getName(), resultHabitDto.getName());
-        assertEquals(habitDto.getDescription(), resultHabitDto.getDescription());
-        assertEquals(habitDto.getCategory(), resultHabitDto.getCategory());
+        List<HabitDto> habitDtos = response.jsonPath().getList("", HabitDto.class);
+        assertNotNull(habitDtos);
+        assertEquals(habitDtos.size(), HABIT_DTOS.size());
+        assertEquals(habitDtos.get(0), HABIT_DTOS.get(0));
+        assertEquals(habitDtos.get(1), HABIT_DTOS.get(1));
     }
 
     @Test
-    public void testGetHabitsByCategory() {
-        final String CATEGORY_STRING = "Category 1";
-        HabitDto habitDto1 = new HabitDto(1L, "Habit_1", "Habit_Description_1", "Category 1");
-        HabitDto habitDto2 = new HabitDto(2L, "Habit_2", "Habit_Description_2", "Category 1");
-        List<HabitDto> habitDtoList = new ArrayList<>(List.of(habitDto1, habitDto2));
+    @DisplayName("GET habit by id")
+    public void shouldReturnHabitWithId() {
+        when(habitFacade.getHabitById(1L)).thenReturn(HABIT_DTO);
 
-        when(habitFacade.getAllHabitsByCategory(CATEGORY_STRING)).thenReturn(habitDtoList);
-        List<HabitDto> resultList = habitController.getHabitsByCategory(CATEGORY_STRING).getBody();
+        MockMvcResponse response = RestAssuredMockMvc.get("/habits/1");
+        response.then().status(HttpStatus.OK);
 
-        assertNotNull(resultList);
-        assertEquals(habitDtoList.size(), resultList.size());
-        assertTrue(resultList.stream().allMatch(habitDto -> CATEGORY_STRING.equals(habitDto.getCategory())));
+        HabitDto habitDto = response.as(HabitDto.class);
+        assertNotNull(habitDto);
+        assertEquals(HABIT_DTO.getId(), habitDto.getId());
+        assertEquals(HABIT_DTO.getName(), habitDto.getName());
+        assertEquals(HABIT_DTO.getDescription(), habitDto.getDescription());
     }
 
     @Test
-    public void testGetHabitsByCategoryWhenCategoryIsNull() {
-        HabitDto habitDto1 = new HabitDto(1L, "Habit_1", "Habit_Description_1", "Category 1");
-        HabitDto habitDto2 = new HabitDto(2L, "Habit_2", "Habit_Description_2", "Category 2");
-        List<HabitDto> habitDtoList = new ArrayList<>(List.of(habitDto1, habitDto2));
+    @DisplayName("GET habits by category")
+    public void whenGetAllHabitsByCategoryThenReturnHabitsMatchingCategory() {
+        when(habitFacade.getAllHabitsByCategory(CATEGORY)).thenReturn(HABIT_DTOS.stream()
+                .filter(habit -> habit.getCategory().equals(CATEGORY))
+                .toList());
 
-        when(habitFacade.getAllHabitsByCategory("")).thenReturn(habitDtoList);
-        List<HabitDto> resultList = habitController.getHabitsByCategory(null).getBody();
+        MockMvcResponse response = RestAssuredMockMvc.given()
+                .queryParam("category", CATEGORY)
+                .get("/habits");
+        response.then().status(HttpStatus.OK);
 
-        assertNotNull(resultList);
-        assertEquals(habitDtoList.size(), resultList.size());
-        assertNotEquals(resultList.get(0).getCategory(), resultList.get(1).getCategory());
-    }
-
-    @Test
-    public void testCreateHabit() {
-        CreateHabitRequest createHabitRequest = new CreateHabitRequest("Habit1", "Habit Description 1", "Main Habits");
-        HabitDto expectedHabitDto = new HabitDto(1L, "Habit1", "Habit Description 1", "Main Habits");
-
-        when(habitFacade.createHabit(createHabitRequest)).thenReturn(expectedHabitDto);
-        HabitDto resultHabitDto = habitController.createHabit(createHabitRequest).getBody();
-
-        assertNotNull(resultHabitDto);
-        assertEquals(resultHabitDto, expectedHabitDto);
-    }
-
-    @Test
-    public void testUpdateHabit() {
-        UpdateHabitRequest updateHabitRequest = new UpdateHabitRequest("Habit1", "Habit Description 1", "Main Habits");
-        HabitDto habitDto = new HabitDto(1L, "Habit1", "Habit Description 1", "Main Habits");
-
-        when(habitFacade.updateHabit(1L, updateHabitRequest)).thenReturn(habitDto);
-        HabitDto resultHabitDto = habitController.updateHabit(1L, updateHabitRequest).getBody();
-
-        assertNotNull(resultHabitDto);
-        assertEquals(habitDto, resultHabitDto);
-    }
-
-    @Test
-    public void testDeleteHabitResultOk() {
-        doNothing().when(habitFacade).deleteHabitById(1L);
-        Object obj = habitController.deleteHabitById(1L);
-        assertNotNull(obj);
-
-        ResponseEntity<String> response = (ResponseEntity<String>) obj;
-        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
-        assertEquals("Habit was successfully deleted.", response.getBody());
-    }
-
-    @Test
-    public void testDeleteHabitThrowHabitNotFoundException() {
-        HabitNotFoundException habitNotFoundException = new HabitNotFoundException("Can not find Habit with id 99!");
-        doThrow(habitNotFoundException).when(habitFacade).deleteHabitById(99L);
-        Object obj = habitController.deleteHabitById(99L);
-        assertNotNull(obj);
-
-        ResponseEntity<String> response = (ResponseEntity<String>) obj;
-        assertEquals(HttpStatusCode.valueOf(404), response.getStatusCode());
-        assertEquals(habitNotFoundException.getMessage(), response.getBody());
-    }
-
-    @Disabled("Disabled for further investigation")
-    @Test
-    public void testDeleteHabitThrowException() {
-        Exception exception = new Exception("Unable to delete Habit!");
-        doThrow(exception).when(habitFacade).deleteHabitById(99L);
-        ResponseEntity<?> response = habitController.deleteHabitById(99L);
-        assertNotNull(response);
-
-        assertEquals(HttpStatusCode.valueOf(500), response.getStatusCode());
-        assertEquals(exception.getMessage(), response.getBody());
+        List<HabitDto> habitDtos = response.jsonPath().getList("", HabitDto.class);
+        assertNotNull(habitDtos);
     }
 }
